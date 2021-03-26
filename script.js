2b1f743177b1dd48dc1aeb09720af8928b596e71
Ru2Lu <jiutianlian0@gmail.com>
2021-03-27 08:33:02 +0900

    Modify text messages and comments.

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
-'use strict';
+"use strict";
 
-const diceEl = document.querySelector('.dice-image');
-const characterEl = document.querySelector('.character');
-const btnRoll = document.getElementById('btn-roll');
-const btnRestart = document.getElementById('btn-restart');
-const message = document.querySelector('.message');
+const diceEl = document.querySelector(".dice-image");
+const characterEl = document.querySelector(".character");
+const btnRoll = document.getElementById("btn-roll");
+const btnRestart = document.getElementById("btn-restart");
+const message = document.querySelector(".message");
 
 let playing = true;
 
-diceEl.classList.add('hidden');
+diceEl.classList.add("hidden");
 
-//Roll dice
+//Roll a dice
 
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     const dice = Math.trunc(Math.random() * 6) + 1;
     console.log(dice);
     diceEl.src = `images/dice-${dice}.png`;
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
 
     let characterSrc = characterEl.src;
     let currentCharacterNum = Number(characterSrc[characterSrc.length - 5]);
 
-    //if roll dice on 1 ,evlolve(current image's index plus 1)
+    //Evolution(If roll a dice on 1 )
     if (dice === 1) {
       characterEl.src = `images/image${currentCharacterNum + 1}.png`;
-      message.textContent = 'EVOLVE!';
-
-      //if roll dice on 2~6 ,disevolve
+      message.textContent = "EVOLUTION!";
     } else {
-      //final character
+      // Be in form final
       if (currentCharacterNum === 6) {
-        message.textContent = "CONGLATULATION! YOU'RE FINAL FORM!!";
-        document.querySelector('body').style.background =
-          'radial-gradient(#01306d, #4cbfdb)';
-        document.querySelector('body').style.color = 'white';
-
+        message.textContent = "CONGLATULATION! YOU'RE IN THE FINAL FORM!!";
+        document.querySelector("body").style.background =
+          "radial-gradient(#01306d, #4cbfdb)";
+        document.querySelector("body").style.color = "white";
+        // Can't roll a dice when in final form
         playing = false;
       }
-      //first character
+      //Degeneration in case of first character
       else if (currentCharacterNum === 1) {
         message.textContent =
-          'If you roll a 1 on the dice,your character will evolve.';
+          "If you roll a 1 on the dice,your character will evolve.";
 
-        //not first character except final character
+        //Degeneration if not first character except the final character
       } else {
-        message.textContent = 'POOR,DISEVOLVE!';
+        message.textContent = "Digeneration ...";
       }
       characterEl.src = `images/image1.png`;
     }
@@ -53,12 +51,12 @@ btnRoll.addEventListener('click', function () {
 });
 
 //Restart the game
-btnRestart.addEventListener('click', function () {
-  diceEl.classList.add('hidden');
+btnRestart.addEventListener("click", function () {
+  diceEl.classList.add("hidden");
   characterEl.src = `images/image1.png`;
   message.textContent =
-    'If you roll a 1 on the dice,your character will evolve.';
-  document.querySelector('body').style.background = 'aqua';
-  document.querySelector('body').style.color = 'black';
+    "If you roll a 1 on the dice,your character will evolve.";
+  document.querySelector("body").style.background = "aqua";
+  document.querySelector("body").style.color = "black";
   playing = true;
 });
